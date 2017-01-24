dba41cb5f6b27b90baa25a58b7cf093aa19aceca
Yifeng Zeng <zeng.yf@live.com>
2017-01-23 23:43:41 -0500

    Complete Q17

--- src/Q17Subsets.java
+++ src/Q17Subsets.java
@@ -2,7 +2,7 @@
  * Created by Yifeng Zeng on 1/22/17.
  */
 import java.util.ArrayList;
-
+import java.util.Arrays;
 /*
 Subsets
 
@@ -26,27 +26,33 @@ class Q17Subsets {
      * @return: A list of lists. All valid subsets.
      */
     public ArrayList<ArrayList<Integer>> subsets(int[] nums) {
-
-
-        ArrayList<Integer> temp = new ArrayList<Integer>();
-        temp.add(123);
-        temp.add(456);
-        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-        result.add(temp);
-//        return result;
-
-        if (nums == null) {
-            return null;
+        Arrays.sort(nums);
+        ArrayList<ArrayList<Integer>> results = new ArrayList<ArrayList<Integer>>();
+        if (nums == null || nums.length == 0) {
+            return results;
         }
 
-        return null;
+        dfsHelper(0, nums, new ArrayList<Integer>(), results);
+        return results;
+    }
 
+    //Add the the all the combination subsets starting from index
+    private void dfsHelper(int index,
+                           int[] nums,
+                           ArrayList<Integer> subset,
+                           ArrayList<ArrayList<Integer>> results) {
+        results.add(new ArrayList<Integer>(subset));
+        for (int i = index; i < nums.length; i++) {
+            subset.add(nums[i]);
+            dfsHelper(i + 1, nums, subset, results);
+            subset.remove(subset.size() - 1);
+        }
     }
 
 
     public static void main(String[] args) {
         Q17Subsets A17 = new Q17Subsets();
-        int[] nums = {1,2,3};
+        int[] nums = {1,3,2};
         ArrayList<ArrayList<Integer>> A17Result = new ArrayList<ArrayList<Integer>>();
         A17Result = A17.subsets(nums);
         if (A17Result != null) {
